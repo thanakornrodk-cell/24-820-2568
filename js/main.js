b951717358e215dd42a70dba303b03f01611e3c0
Thanakorn <thanakorn.rodk@ku.th>
2025-12-29 19:41:01 +0700

    js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*
+loop statements:
+while
+for
+*/
+
+let counter = -5;
+while (counter <10) {
+    console.log('Hello World ');
+    //counter = counter + 1;
+    counter += 1;
+}
+
+for (let i=0; i<10; i=i++) {
+    console.log('Hello World from for loop');
+    
+}
